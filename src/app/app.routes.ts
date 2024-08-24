233c90e58c6d1ba78954ab90f318bb79a6e0d8d9
ahmed.kamar <ahmed.shaban.kamar@gmail.com>
2024-08-24 09:24:10 +0300

    feat(product): add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { SearchComponent } from './components/products/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SearchComponent, pathMatch: 'full' },
+  {
+    path: 'product/:id',
+    loadComponent: () =>
+      import(
+        './components/products/product-details/product-details.component'
+      ).then((m) => m.ProductDetailsComponent),
+  },
+  { path: '**', redirectTo: '' },
+];
