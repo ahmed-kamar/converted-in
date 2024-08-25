4ee6f4c49fd9cba54bcf0b883f5173fc40531786
ahmed.kamar <ahmed.shaban.kamar@gmail.com>
2024-08-25 04:33:16 +0300

    feat(product): add menu elements

--- src/app/components/products/search/search.component.ts
+++ src/app/components/products/search/search.component.ts
@@ -3,24 +3,46 @@ import { ProductService } from '../../../core/services/product.service';
 import { Product } from '../../../core/models/product.model';
 import { NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { Category } from '../../../core/models/category.model';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
+import { SliderModule } from 'primeng/slider';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [NgOptimizedImage, RouterLink],
+  imports: [
+    NgOptimizedImage,
+    RouterLink,
+    FormsModule,
+    CheckboxModule,
+    SliderModule,
+    PaginatorModule,
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
 })
 export class SearchComponent {
   products: Product[] = [];
+  categories: Category[] = [];
+  selectedBrands = [];
+  rangeValues: number[] = [1, 5];
 
   constructor(private productService: ProductService) {
     this.getProducts();
+    this.getCategories();
   }
 
   private getProducts() {
     this.productService.getProducts().subscribe((data) => {
       this.products = data;
+    });
+  }
+
+  getCategories() {
+    this.productService.getCategories().subscribe((data) => {
+      this.categories = data;
     });
   }
 }
