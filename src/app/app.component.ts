8b62dff673c6084bac7ac2bc0d561bb004b29f27
ahmed.kamar <ahmed.shaban.kamar@gmail.com>
2024-08-25 06:19:23 +0300

    feat(breadcrump): add breadcrump

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/components/header/header.component';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { MenuItem } from 'primeng/api';
+import { NgClass, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    BreadcrumbModule,
+    NgClass,
+    NgOptimizedImage,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'commerce-platform';
+  items: MenuItem[] | undefined;
+
+  home: MenuItem | undefined;
+
+  constructor() {
+    this.items = [
+      { icon: 'pi pi-home', route: '/' },
+      { label: 'Electronics & Mobiles', route: '/' },
+      {
+        label: 'Sony PlayStation 5 DigitalEdition Console With Controller',
+        route: '/',
+      },
+    ];
+  }
 }
