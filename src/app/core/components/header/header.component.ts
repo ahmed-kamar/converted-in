0dea49f93805333e20d615590fbad224e54db48d
ahmed.kamar <ahmed.shaban.kamar@gmail.com>
2024-08-25 03:26:08 +0300

    feat(product): add search and product details styling

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,12 +1,25 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { Category } from '../../models/category.model';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  categories: Category[] = [];
 
+  constructor(private productService: ProductService) {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.productService.getCategories().subscribe((data) => {
+      this.categories = data;
+    });
+  }
 }
